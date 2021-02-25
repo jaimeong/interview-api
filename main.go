203086083a424e10bab03efddfa3e7eb17beb6a3
jimmyjongs <jdojimmy@gmail.com>
2021-02-24 21:06:30 -0800

    implement structs, user api

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 
+	"math/rand"
+
 	"log"
 
 	"net/http"
@@ -11,129 +13,166 @@ import (
 
 	"fmt"
 
-	"math/rand"
-
 	"strconv"
 )
 
-// book struct (model)
-type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+// User model
+type User struct {
+	ID         string      `json:"id"`
+	Firstname  string      `json:"firstname"`
+	Lastname   string      `json:"lastname"`
+	Interviews []Interview `json:"interviews"`
 }
 
-// author struct
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
+// Interview model
+type Interview struct {
+	Date  string  `json:"date`
+	Party []User  `json:"party"`
+	Score float32 `json:"score"`
 }
 
-// init books var as a slice (list) of book structs
-var books []Book
+// Question model
+type Question struct {
+	Category string  `json:"category"`
+	Question string  `json:"question"`
+	Answer   string  `json:"answer"`
+	Score    float32 `json:score`
+}
 
-func getBooks(w http.ResponseWriter, r *http.Request) {
+// init users as User slice (list)
+var users []User
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
 	// set header
 	w.Header().Set("Content-Type", "application/json")
 
-	//encode books into json
-	json.NewEncoder(w).Encode(books)
+	//encode users into json
+	json.NewEncoder(w).Encode(users)
 }
 
-func getBook(w http.ResponseWriter, r *http.Request) {
+func getUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// get params
 	params := mux.Vars(r)
 
-	// loop thhru books, find with id
-	// for _ + for item in books
-	for _, item := range books {
+	// loop thhru users, find with id
+	// for _ + for item in users
+	for _, item := range users {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
 
-	json.NewEncoder(w).Encode(&Book{})
+	json.NewEncoder(w).Encode(&User{})
 
 }
 
-func createBook(w http.ResponseWriter, r *http.Request) {
+func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
 
-	book.ID = strconv.Itoa(rand.Intn(1000000))
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
+	user.ID = strconv.Itoa(rand.Intn(1000000))
+
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
 
 }
 
-// combintation of delete + create
-func updateBook(w http.ResponseWriter, r *http.Request) {
+// // combintation of delete + create
+func updateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// get params
 	params := mux.Vars(r)
 
-	for index, item := range books {
+	for index, item := range users {
 		if item.ID == params["id"] {
 			// delete's slice
-			books = append(books[:index], books[index+1:]...)
+			users = append(users[:index], users[index+1:]...)
 
-			// creates creation
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
+			// create's creation
+			var user User
+			_ = json.NewDecoder(r.Body).Decode(&user)
 
-			book.ID = params["id"]
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
+			user.ID = params["id"]
+			users = append(users, user)
+			json.NewEncoder(w).Encode(user)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(users)
 }
 
-func deleteBook(w http.ResponseWriter, r *http.Request) {
+func deleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// get params
 	params := mux.Vars(r)
 
-	for index, item := range books {
+	for index, item := range users {
 		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
+			users = append(users[:index], users[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(users)
 }
+
+// generate dummy interview data
+func genInterviews(n int) []Interview {
+	// generate interviews
+	tmp := make([]Interview, n)
+	usersList := make([]User, 1)
+	usersList = append(usersList, users...)
+
+	for i := range tmp {
+		tmp[i].Date = strconv.Itoa(rand.Intn(100))
+		tmp[i].Party = usersList
+		tmp[i].Score = rand.Float32() * 5
+	}
+	return tmp
+}
+
+//  choose random user from users slice
 
 func main() {
 	// init mux router
 	router := mux.NewRouter()
 
 	// dummy data @ todo - implement DB
-	books = append(books, Book{
-		ID:     "1",
-		Isbn:   "34561",
-		Title:  "Book1",
-		Author: &Author{Firstname: "John", Lastname: "Smith"}})
 
-	books = append(books, Book{
-		ID:     "2",
-		Isbn:   "77861",
-		Title:  "Book22",
-		Author: &Author{Firstname: "Jane", Lastname: "Moe"}})
+	users = append(users, User{
+		ID:         "1",
+		Firstname:  "John",
+		Lastname:   "Smith",
+		Interviews: genInterviews(2),
+	})
 
-	// route handlers / endpoints
-	router.HandleFunc("/api/books", getBooks).Methods("GET")
-	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/api/books", createBook).Methods("POST")
-	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	users = append(users, User{
+		ID:         "2",
+		Firstname:  "Jane",
+		Lastname:   "Doe",
+		Interviews: genInterviews(1),
+	})
+
+	users = append(users, User{
+		ID:         "3",
+		Firstname:  "Sam",
+		Lastname:   "Thi",
+		Interviews: genInterviews(3),
+	})
+
+	// @ TODO: Figure out interview struct inside User struct
+
+	// user route handlers / endpoints
+	router.HandleFunc("/api/users", getUsers).Methods("GET")
+	router.HandleFunc("/api/user/{id}", getUser).Methods("GET")
+	router.HandleFunc("/api/user", createUser).Methods("POST")
+	router.HandleFunc("/api/user/{id}", updateUser).Methods("PUT")
+	router.HandleFunc("/api/user/{id}", deleteUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 	fmt.Println("Server listening on :8000")
