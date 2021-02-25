c14fec829bfed41511b7918f161a4e5dc466df1f
jimmyjongs <jdojimmy@gmail.com>
2021-02-25 13:07:24 -0800

    implement interview api

--- models/Interview.go
+++ models/Interview.go
@@ -1,6 +1,7 @@
 package models
 
 type Interview struct {
+	ID    string  `json:"id"`
 	Date  string  `json:"date`
 	Party []User  `json:"party"`
 	Score float32 `json:"score"`
--- models/User.go
+++ models/User.go
@@ -1,8 +1,9 @@
 package models
 
 type User struct {
-	ID         string      `json:"id"`
-	Firstname  string      `json:"firstname"`
-	Lastname   string      `json:"lastname"`
-	Interviews []Interview `json:"interviews"`
+	ID        string `json:"id"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Usernamee string `json:"username"`
+	Password  string `json:"password"`
 }
