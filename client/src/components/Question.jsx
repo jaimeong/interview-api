5b423e97e5d2bed757a2caf10ca6af7b20e5432b
henrytran721 <henryhtran07@gmail.com>
2021-03-01 10:17:36 -0800

    frontend api

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -6,21 +6,47 @@ const Question = function({setData, questionData, index, setQuestionData}) {
     const questionBox = useRef(null);
     const responseBox = useRef(null);
     const notesBox = useRef(null);
+    const questionContainer = useRef(null);
     const [question, setQuestion] = useState('');
     const [response, setResponse] = useState('');
     const [notes, setNotes] = useState('');
     const [saved, setSaved] = useState(false);
     function handleSave() {
-        let type = questionType.current.value;
-        let data = {id: questionData.length, type: type, question: question, response: response, notes: notes};
-        setData(data);
-        questionBox.current.readOnly = true;
-        questionBox.current.style.border = '2px solid green';
-        responseBox.current.readOnly = true;
-        responseBox.current.style.border = '2px solid green';
-        notesBox.current.readOnly = true;
-        notesBox.current.style.border = '2px solid green';
-        setSaved(true);
+        let current = parseInt(questionContainer.current.id, 10);
+        if(current >= questionData.length) {
+            let type = questionType.current.value;
+            let data = {id: questionData.length, type: type, question: question, response: response, notes: notes};
+            setData(data);
+            questionBox.current.readOnly = true;
+            questionBox.current.style.border = '2px solid green';
+            responseBox.current.readOnly = true;
+            responseBox.current.style.border = '2px solid green';
+            notesBox.current.readOnly = true;
+            notesBox.current.style.border = '2px solid green';
+            questionType.current.disabled = true;
+            setSaved(true);
+        } else {
+            setQuestion(questionBox.current.value);
+            setResponse(responseBox.current.value);
+            setNotes(notesBox.current.value);
+            let type = questionType.current.value;
+            let data = {id: index, type: type, question: question, response: response, notes: notes};
+            for(let i = 0; i < questionData.length; i++) {
+                if(questionData[i].id === current) {
+                    questionData[i] = data;
+                }
+            }
+
+            questionBox.current.readOnly = true;
+            questionBox.current.style.border = '2px solid green';
+            responseBox.current.readOnly = true;
+            responseBox.current.style.border = '2px solid green';
+            notesBox.current.readOnly = true;
+            notesBox.current.style.border = '2px solid green';
+            questionType.current.disabled = true;
+
+            setSaved(true);
+        }
     }
 
     function handleUpdate() {
@@ -32,22 +58,23 @@ const Question = function({setData, questionData, index, setQuestionData}) {
         responseBox.current.style.border = '2px solid gold';
         notesBox.current.readOnly = false;
         notesBox.current.style.border = '2px solid gold';
+        questionType.current.disabled = false;
     }
 
     function handleChange(e, operation) {
         operation(e.target.value);
     }
     return (
-        <div className='question-container'>
+        <div className='question-container' id={index} ref={questionContainer}>
             <select ref={questionType}>
                 <option value='conversational'>Conversational</option>
                 <option value='conceptual'>Conceptual</option>
                 <option value='technical'>Technical</option>
             </select>
-            <textarea type='text' placeholder='Enter your question' name='question' rows="4" cols="50" ref={questionBox} onChange={(e) => handleChange(e, setQuestion)}></textarea>
-            <textarea type='text' placeholder='Enter their response' name='response' rows="4" cols="50" ref={responseBox} onChange={(e) => handleChange(e, setResponse)}></textarea>
-            <textarea type='text' placeholder='Enter your notes for this question' name='notes' rows="4" cols="50" ref={notesBox} onChange={(e) => handleChange(e, setNotes)}></textarea>
-            {!saved ? <button onClick={handleSave}>Save</button> : <button onClick={handleUpdate}>Update</button>}
+            <textarea type='text' placeholder='Enter your question' name='question' rows="4" cols="50" ref={questionBox} value={question} onChange={(e) => handleChange(e, setQuestion)}></textarea>
+            <textarea type='text' placeholder='Enter their response' name='response' rows="4" cols="50" ref={responseBox} value={response} onChange={(e) => handleChange(e, setResponse)}></textarea>
+            <textarea type='text' placeholder='Enter your notes for this question' name='notes' rows="4" cols="50" ref={notesBox} value={notes} onChange={(e) => handleChange(e, setNotes)}></textarea>
+            {!saved ? <button className='saved-btn' onClick={handleSave}>Save</button> : <button className='update-btn' onClick={handleUpdate}>Update</button>}
         </div>
     )
 }
