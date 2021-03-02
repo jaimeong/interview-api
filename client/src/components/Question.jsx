c94f86ffcf8c29c738c68c31d6e5816e68081a14
henrytran721 <henryhtran07@gmail.com>
2021-03-02 14:06:46 -0800

    added scores

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -6,11 +6,13 @@ const Question = function({setData, questionData, index, setQuestionData}) {
     const questionBox = useRef(null);
     const responseBox = useRef(null);
     const notesBox = useRef(null);
+    const scoreInput = useRef(null);
     const questionContainer = useRef(null);
     const error = useRef(null);
     const [question, setQuestion] = useState('');
     const [response, setResponse] = useState('');
     const [notes, setNotes] = useState('');
+    const [score, setScore] = useState('');
     const [saved, setSaved] = useState(false);
     function handleSave() {
         let current = parseInt(questionContainer.current.id, 10);
@@ -21,7 +23,7 @@ const Question = function({setData, questionData, index, setQuestionData}) {
             notesBox.current.style.border = '2px solid red';
         } else if(current >= questionData.length) {
             let type = questionType.current.value;
-            let data = {id: questionData.length, type: type, question: question, response: response, notes: notes, date: new Date()};
+            let data = {id: questionData.length, type: type, question: question, response: response, notes: notes, date: new Date(), score: score};
             setData(data);
             console.log(questionBox);
             questionBox.current.readOnly = true;
@@ -38,7 +40,8 @@ const Question = function({setData, questionData, index, setQuestionData}) {
             setResponse(responseBox.current.value);
             setNotes(notesBox.current.value);
             let type = questionType.current.value;
-            let data = {id: index, type: type, question: question, response: response, notes: notes};
+            let data = {id: index, type: type, question: question, response: response, notes: notes, score: score};
+            console.log(data);
             for(let i = 0; i < questionData.length; i++) {
                 if(questionData[i].id === current) {
                     data.date = questionData[i].date;
@@ -84,6 +87,7 @@ const Question = function({setData, questionData, index, setQuestionData}) {
             <textarea type='text' placeholder='Enter your question' name='question' rows="4" cols="50" ref={questionBox} value={question} onChange={(e) => handleChange(e, setQuestion)}></textarea>
             <textarea type='text' placeholder='Enter their response' name='response' rows="4" cols="50" ref={responseBox} value={response} onChange={(e) => handleChange(e, setResponse)}></textarea>
             <textarea type='text' placeholder='Enter your notes for this question' name='notes' rows="4" cols="50" ref={notesBox} value={notes} onChange={(e) => handleChange(e, setNotes)}></textarea>
+            <input type="number" max="5" onChange={(e) => handleChange(e, setScore)} ref={scoreInput} />
             {!saved ? <button className='saved-btn' onClick={handleSave}>Save</button> : <button className='update-btn' onClick={handleUpdate}>Update</button>}
             <p className='errorMsg' ref={error}></p>
         </div>
