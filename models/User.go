1254c806eac1a5a218c44516ce17796664f47032
jimmyjongs <jdojimmy@gmail.com>
2021-03-04 15:21:49 -0800

    finalize interview model

--- models/User.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ID        string `json:"id"`
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-	Usernamee string `json:"username"`
-	Password  string `json:"password"`
-}
