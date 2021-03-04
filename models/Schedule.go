340bb5fba14aa19e09e488b5575f6ba8c0e320ce
jimmyjongs <jdojimmy@gmail.com>
2021-03-04 15:50:04 -0800

    add schedule model, endpoints

---
+++ models/Schedule.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Schedule []struct {
+	CalendarID string    `json:"calendarId"`
+	Category   string    `json:"category"`
+	Title      string    `json:"title"`
+	Start      time.Time `json:"start"`
+	End        time.Time `json:"end"`
+	ID         string    `json:"id"`
+	BgColor    string    `json:"bgColor"`
+	IsVisible  bool      `json:"isVisible"`
+	Location   string    `json:"location"`
+}
