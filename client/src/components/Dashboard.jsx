c2f70f9e288dd01f8c85549c0f96d09bb95fb205
henrytran721 <henryhtran07@gmail.com>
2021-03-04 10:32:02 -0800

    refactored frontend for data formatting

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,14 +1,89 @@
 import React, { useState } from 'react';
 import '../sass/index.scss';
 
-let initialState = [
-    {id: 0, interviewer: 'Henry', interviewee: 'Jimmy', date: new Date()},
-    {id: 1, interviewer: 'Johnny', interviewee: 'Jimmy', date: new Date()},
-    {id: 2, interviewer: 'Albert', interviewee: 'Evan', date: new Date()},
-    {id: 3, interviewer: 'Evan', interviewee: 'Jimmy', date: new Date()},
+let initialState =  [
+    {
+        id: "utgt-JfIs3",
+        interviewee: "Henry",
+        date: "2021-03-03T01:36:13.895Z",
+        interviewer: "Johnny",
+        questionData: [
+            {
+                id: 0,
+                type: "conversational",
+                question: "sdfsd",
+                response: "sdsd",
+                notes: "sdsdfsdfsd",
+                date: "2021-03-03T01:36:08.767Z",
+                score: "3"
+            },
+            {
+                id: 1,
+                type: "conversational",
+                question: "asdasd",
+                response: "asdasd",
+                notes: "asdasda",
+                date: "2021-03-03T01:36:12.353Z",
+                score: "1"
+            }
+        ]
+},
+{
+    id: "utgt-JfIs3",
+    interviewee: "Albert",
+    date: "2021-03-03T01:36:13.895Z",
+    interviewer: "Evan",
+    questionData: [
+        {
+            id: 0,
+            type: "conversational",
+            question: "sdfsd",
+            response: "sdsd",
+            notes: "sdsdfsdfsd",
+            date: "2021-03-03T01:36:08.767Z",
+            score: "3"
+        },
+        {
+            id: 1,
+            type: "conversational",
+            question: "asdasd",
+            response: "asdasd",
+            notes: "asdasda",
+            date: "2021-03-03T01:36:12.353Z",
+            score: "1"
+        }
+    ]
+},
+{
+    id: "utgt-JfIs3",
+    interviewee: "Johnny",
+    date: "2021-03-03T01:36:13.895Z",
+    interviewer: "Jimmy",
+    questionData: [
+        {
+            id: 0,
+            type: "conversational",
+            question: "sdfsd",
+            response: "sdsd",
+            notes: "sdsdfsdfsd",
+            date: "2021-03-03T01:36:08.767Z",
+            score: "3"
+        },
+        {
+            id: 1,
+            type: "conversational",
+            question: "asdasd",
+            response: "asdasd",
+            notes: "asdasda",
+            date: "2021-03-03T01:36:12.353Z",
+            score: "1"
+        }
+    ]
+}
 ]
 
 const Dashboard = () => {
+    console.log(initialState);
     return (
         <div className='dashboard-container'>
             <h1 className='interview-header'>Horse Field Interviews</h1>
@@ -20,7 +95,7 @@ const Dashboard = () => {
                     <a className='interview-card' key={id} href={`/api/interviews/` + interview.id}>
                         <p>Interviewee: {interview.interviewee}</p>
                         <p>Interviewer: {interview.interviewer}</p>
-                        <p>{interview.date.toString().slice(0, 15)}</p>
+                        <p>{interview.date.toString().slice(0, 10)}</p>
                     </a>
                 )
             })}
